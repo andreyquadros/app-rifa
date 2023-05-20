3c1fa72e31ca4f20ff090a146afa5e1de65f1f61
RICARDO OLIVEIRA <ricardo.oliveira@popcode.com.br>
2023-05-19 21:19:43 -0400

    refactor: Atualizando o app para funcinar corretamente

--- lib/view/screens/webview_screen.dart
+++ lib/view/screens/webview_screen.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:rifa_flutter/view_model/ScreenArguments.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -14,32 +12,26 @@ class WebViewScreen extends StatefulWidget {
 
   static const routeName = '/webview';
 
-
   @override
   State<WebViewScreen> createState() => _WebViewScreenState();
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-
-
-
   Future<void> _abrirURL(String uri) async {
     print('o link é: ${uri}');
     final Uri _url = Uri.parse(uri);
     print(_url);
-    
 
     if (!await launchUrl(_url)) {
       throw 'Não pode inicializar $_url';
-    }else{
+    } else {
       await launchUrl(_url);
       print('é pentaaaaaaaaaaa!');
     }
   }
 
   final Completer<WebViewController> _controller =
-  Completer<WebViewController>();
-
+      Completer<WebViewController>();
 
   @override
   void initState() {
@@ -56,34 +48,30 @@ class _WebViewScreenState extends State<WebViewScreen> {
 
     print('ooooooooooooooooooooi');
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("App Rifa - IFRO"),
-      ),
-      body: WebView(
-        initialUrl: "https://api.whatsapp.com/send/?phone=5569993485858",
-        javascriptMode: JavascriptMode.disabled,
-        navigationDelegate: (NavigationRequest request) async {
-          if (request.url
-              .startsWith('https://api.whatsapp.com/send/?phone=')) {
-            print('blocking navigation to $request}');
-            List<String> urlSplitted = request.url.split("&text=");
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("App Rifa - IFRO"),
+        ),
+        body: WebView(
+          initialUrl: "https://api.whatsapp.com/send/?phone=5569993485858",
+          javascriptMode: JavascriptMode.disabled,
+          navigationDelegate: (NavigationRequest request) async {
+            if (request.url
+                .startsWith('https://api.whatsapp.com/send/?phone=')) {
+              print('blocking navigation to $request}');
+              List<String> urlSplitted = request.url.split("&text=");
 
-            String phone = '5569993485858';
-            String message =
-            urlSplitted.last.toString().replaceAll("%20", " ");
+              String phone = '5569993485858';
+              String message =
+                  urlSplitted.last.toString().replaceAll("%20", " ");
 
-            await _abrirURL(
-                "${Uri.parse(message)}");
-            return NavigationDecision.prevent;
-          }
+              await _abrirURL("${Uri.parse(message)}");
+              return NavigationDecision.prevent;
+            }
 
-          print('allowing navigation to $request');
-          return NavigationDecision.navigate;
-        },
-
-      )
-    );
+            print('allowing navigation to $request');
+            return NavigationDecision.navigate;
+          },
+        ));
   }
-
 }
--- lib/view/widgets/rifa.dart
+++ lib/view/widgets/rifa.dart
@@ -1,16 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'package:rifa_flutter/model/VendedoresMock.dart';
 import 'package:rifa_flutter/model/class/Comprador.dart';
-import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:pdf/widgets.dart' as pw;
-import '../../firebase_options.dart';
-
 
 enum Share {
   whatsapp,
@@ -27,7 +26,6 @@ class RifaWidget extends StatefulWidget {
 
 class _RifaWidgetState extends State<RifaWidget> {
   @override
-
   String? _nome;
   String? _telefone;
   String? _endereco;
@@ -39,7 +37,7 @@ class _RifaWidgetState extends State<RifaWidget> {
   TextEditingController _telefoneController = TextEditingController();
   TextEditingController _enderecoController = TextEditingController();
 
-  void _limparControladores(){
+  void _limparControladores() {
     _nomeController.clear();
     _telefoneController.clear();
     _enderecoController.clear();
@@ -47,18 +45,18 @@ class _RifaWidgetState extends State<RifaWidget> {
 
   String? _validarNome(String? value) {
     if (value?.isEmpty ?? false) {
-
       const snackBar = SnackBar(
-        content: Text('Por favor preencha o campo Nome e Telefone, eles são obrigatórios!'),
+        content: Text(
+            'Por favor preencha o campo Nome e Telefone, eles são obrigatórios!'),
       );
 
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
     }
     final RegExp nameExp = RegExp(r'^[A-Za-z ]+$');
     if (!nameExp.hasMatch(value!)) {
       const snackBar = SnackBar(
-        content: Text('Utilize apenas letras do alfabeto, sem caracteres especiais e também não deixe vazio!'),
+        content: Text(
+            'Utilize apenas letras do alfabeto, sem caracteres especiais e também não deixe vazio!'),
       );
 
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
@@ -68,12 +66,19 @@ class _RifaWidgetState extends State<RifaWidget> {
 
   void _salvarFirebase() async {
     final FirebaseFirestore db = FirebaseFirestore.instance;
-    String _nomeC  = _nomeController.text;
+    String _nomeC = _nomeController.text;
     String _telefoneC = _telefoneController.text;
     String _enderecoC = _enderecoController.text;
 
-    Comprador _comprador = Comprador(nome:_nomeC, telefone: _telefoneC, endereco: _enderecoC, vendedor: vendedorFinal );
-    await db.collection("compradores").add(_comprador.toMap()).then((DocumentReference doc) {
+    Comprador _comprador = Comprador(
+        nome: _nomeC,
+        telefone: _telefoneC,
+        endereco: _enderecoC,
+        vendedor: vendedorFinal);
+    await db
+        .collection("compradores")
+        .add(_comprador.toMap())
+        .then((DocumentReference doc) {
       gerarPdf(doc.id, _nomeC, _telefoneC, _enderecoC, vendedorFinal);
       return print(doc.id);
     });
@@ -83,10 +88,10 @@ class _RifaWidgetState extends State<RifaWidget> {
     );
 
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
   }
 
-  Future<void> gerarPdf(String doc, String nome, String telefone, String endereco, String vendedor) async {
+  Future<void> gerarPdf(String doc, String nome, String telefone,
+      String endereco, String vendedor) async {
     final font = await rootBundle.load("fonts/open-sans.ttf");
     final ttf = pw.Font.ttf(font);
 
@@ -94,44 +99,64 @@ class _RifaWidgetState extends State<RifaWidget> {
     final pw.Document pdf = pw.Document(deflate: zlib.encode);
 
     pdf.addPage(pw.MultiPage(
-        theme: pw.ThemeData(defaultTextStyle: pw.TextStyle(font: ttf) ),
+        theme: pw.ThemeData(defaultTextStyle: pw.TextStyle(font: ttf)),
         build: (context) {
-      return [
-      pw.Table.fromTextArray(context: context, data:  <List<String>>[
-        ['Nome' , 'Telefone' , 'Endereço' , 'Qual a sala fez a venda?' , 'Hash Identificador'],
-        ['${nome}' , '${telefone}' , '${endereco}' , '${vendedor}' , '${doc}']
-      ],
-      ),
-    ];
-    }));
+          return [
+            pw.Table.fromTextArray(
+              context: context,
+              data: <List<String>>[
+                [
+                  'Nome',
+                  'Telefone',
+                  'Endereço',
+                  'Qual a sala fez a venda?',
+                  'Hash Identificador'
+                ],
+                [
+                  '${nome}',
+                  '${telefone}',
+                  '${endereco}',
+                  '${vendedor}',
+                  '${doc}'
+                ]
+              ],
+            ),
+          ];
+        }));
 
-      final output = await getExternalStorageDirectory();
-      final file = File('${output?.path}/rifa-${doc}.pdf');
-      await file.writeAsBytes(await pdf.save());
-      print(output?.path);
-      print('PDF SALVO COM SUCESSO!');
-      if (output != null) {
-        await OpenFile.open('${output?.path}/rifa-${doc}.pdf');
-      }
-      //await enviarWhatsApp(file, output, telefone);
-      print('PDF ABERTO COM SUCESSO!!');
+    final output = await getExternalStorageDirectory();
+    final file = File('${output?.path}/rifa-${doc}.pdf');
+    await file.writeAsBytes(await pdf.save());
+    print(output?.path);
+    print('PDF SALVO COM SUCESSO!');
+    if (output != null) {
+      await OpenFile.open('${output.path}/rifa-${doc}.pdf');
+    }
+    //await enviarWhatsApp(file, output, telefone);
+    print('PDF ABERTO COM SUCESSO!!');
     _limparControladores();
-
   }
 
   @override
-  void initState(){
+  void initState() {
     _databaseFB;
   }
-  Future <void> _databaseFB() async{
-    final database = await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+
+  Future<void> _databaseFB() async {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "Api key here",
+        appId: "App id here",
+        messagingSenderId: "Messaging sender id here",
+        projectId: "project id here",
+      ),
     );
   }
 
   _launchWhatsapp(String telefone, String comprador) async {
     var whatsapp_number = "+55${telefone}";
-    var whatsappAndroid =Uri.parse("whatsapp://send?phone=$whatsapp_number&text=Olá, ${comprador}! Tudo bem?! Vou enviar seu comprovante de compra de rifa! Obrigado por ajudar o IFRO Campus Ariquemes!");
+    var whatsappAndroid = Uri.parse(
+        "whatsapp://send?phone=$whatsapp_number&text=Olá, ${comprador}! Tudo bem?! Vou enviar seu comprovante de compra de rifa! Obrigado por ajudar o IFRO Campus Ariquemes!");
     if (await canLaunchUrl(whatsappAndroid)) {
       await launchUrl(whatsappAndroid);
     } else {
@@ -144,7 +169,6 @@ class _RifaWidgetState extends State<RifaWidget> {
   }
 
   Widget build(BuildContext context) {
-
     return SingleChildScrollView(
       child: SafeArea(
         child: Padding(
@@ -204,47 +228,53 @@ class _RifaWidgetState extends State<RifaWidget> {
               const SizedBox(height: 30.0),
               ElevatedButton.icon(
                   style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      textStyle: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold)),
+                      backgroundColor: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      textStyle:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
                   onPressed: () async {
                     _validarNome(_nomeController.text);
                     if (_telefoneController.text.isNotEmpty)
-                    _launchWhatsapp(_telefoneController.text, _nomeController.text);
-                    },
-                    icon: Icon(Icons.whatsapp),
+                      _launchWhatsapp(
+                          _telefoneController.text, _nomeController.text);
+                  },
+                  icon: Icon(Icons.wifi_calling_3),
                   label: Text('Abrir contato no WhatsApp')),
               const SizedBox(height: 20.0),
               ElevatedButton.icon(
                   style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      textStyle: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold)),
-                  onPressed: (){
+                      backgroundColor: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      textStyle:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  onPressed: () {
                     _validarNome(_nomeController.text);
-                    if (_nomeController.text.isNotEmpty)
-                    _salvarFirebase();},
+                    if (_nomeController.text.isNotEmpty) _salvarFirebase();
+                  },
                   icon: Icon(Icons.sell),
-                  label: Text('Gerar Rifa',)),
+                  label: Text(
+                    'Gerar Rifa',
+                  )),
               const SizedBox(height: 40.0),
-              Text('Primeiro envie uma mensagem para o contato e depois gere a Rifa.')
+              Text(
+                  'Primeiro envie uma mensagem para o contato e depois gere a Rifa.')
             ],
           ),
         ),
       ),
     );
   }
+
   Widget _buildVendorSelector() {
     final dropdown = DropdownButton<VendedoresMock>(
       value: _vendedoresMock,
       onChanged: (novoVendedor) {
-       vendedorFinal = novoVendedor.toString();
+        vendedorFinal = novoVendedor.toString();
 
-        if (novoVendedor != null) setState(() => _vendedoresMock = novoVendedor);
+        if (novoVendedor != null)
+          setState(() => _vendedoresMock = novoVendedor);
       },
       items: [
         for (final vendedor in VendedoresMock.values)
